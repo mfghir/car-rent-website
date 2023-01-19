a9b5e32288c448020bc9ee4361187662e4997660
miss-f-g <fatemeghafari98@gmail.com>
2023-01-19 15:41:36 +0330

    add style to detalCar

--- src/components/DetailCar.jsx
+++ src/components/DetailCar.jsx
@@ -37,13 +37,15 @@ const DetailCar = () => {
               views,
               reviewer,
               caption,
-              typecar,
+              typeCar,
               capacity,
               fuel,
+              price,
+              reviews,
             } = item;
             return (
-              <div key={id} className="w-full bg-red-200">
-                <section className="flex flex-wrap bg-red-900 ">
+              <div key={id} className="w-full">
+                <section className="flex flex-wrap">
                   <img
                     className="rounded-lg"
                     src="https://upcdn.io/kW15b2b/raw/phone-categoryAds-ZkC2.png"
@@ -56,9 +58,9 @@ const DetailCar = () => {
                   </div>
                 </section>
 
-                <section className="mt-8 bg-white p-4 ">
+                <section className="mt-8 bg-white p-4 rounded-[10px]">
                   <h1 className="text-[#1A202C] font-bold text-xl">{name}</h1>
-                  <div className="flex justify-between items-center w-[60%]  mt-[6px]">
+                  <div className="flex justify-between items-center w-[64%]  mt-[6px]">
                     <div className="flex justify-between items-center">
                       <Star1 size="12" color="#fbad39" variant="Bold" />
                       <Star1 size="12" color="#fbad39" variant="Bold" />
@@ -76,20 +78,68 @@ const DetailCar = () => {
                     {caption}
                   </p>
 
-                  <ul className="flex justify-between items-center flex-wrap ">
-                    <li>
-                      <span>Type Car </span> {typecar}
+                  <ul className="flex justify-between items-center flex-wrap text-xs mt-4">
+                    <li className="w-[45%] flex justify-between items-center ">
+                      <span className="text-[#90A3BF] font-medium block">
+                        Type Car
+                      </span>
+                      <span className="text-[#1A202C] font-semibold block">
+                        {typeCar}
+                      </span>
                     </li>
-                    <li>
-                      <span>Capacity</span> {capacity}
+                    <li className="w-[45%] flex justify-between items-center ">
+                      <span className="text-[#90A3BF] font-medium">
+                        Capacity
+                      </span>
+                      <span className="text-[#1A202C] font-semibold">
+                        {capacity}Person
+                      </span>
                     </li>
-                    <li>
-                      <span>Steering</span> Manual
+                    <li className="w-[45%] flex justify-between items-center mt-4">
+                      <span className="text-[#90A3BF] font-medium">
+                        Steering
+                      </span>
+                      <span className="text-[#1A202C] font-semibold">
+                        Manual
+                      </span>
                     </li>
-                    <li>
-                      <span>Gasoline</span> {fuel}
+                    <li className="w-[45%] flex justify-between items-center mt-4">
+                      <span className="text-[#90A3BF] font-medium">
+                        Gasoline
+                      </span>
+                      <span className="text-[#1A202C] font-semibold">
+                        {fuel}
+                      </span>
                     </li>
                   </ul>
+
+                  <div className="flex justify-between items-center mt-8 ">
+                    <div>
+                      <section className="flex justify-between items-end font-bold">
+                        <div className="text-[#1A202C] text-xl ">
+                          ${`${Math.floor(price - price * 0.2)}`}.00/
+                        </div>
+                        <span className="text-[#90A3BF] text-xs">days</span>
+                      </section>
+
+                      <p className="text-xs mt-1 line-through">${price}.00</p>
+                    </div>
+
+                    <button className="bg-[#3563E9] rounded-[4px] text-white text-xs lg:text-base font-semibold px-4 py-[10px] lg:px-5">
+                      Rental Now
+                    </button>
+                  </div>
+                </section>
+
+                <section className="mt-8 bg-white p-4 rounded-[10px]">
+                  <div className="flex justify-between items-center">
+                    <p className="text-[#1A202C] font-semibold text-xl">
+                      Reviews
+                    </p>
+                    <span className="bg-[#3563E9] rounded-[4px] text-white text-sm font-bold px-4 py-2">
+                      12{reviews.length}
+                    </span>
+                  </div>
                 </section>
               </div>
             );
